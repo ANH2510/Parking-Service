7e849bc18f858ed27f76852cc77447be21bc7802
ANH2510 <nguyenlexuananh2510@gmail.com>
2022-04-13 14:20:07 +0200

    2

--- challenge_2.sql
+++ challenge_2.sql
@@ -6,9 +6,9 @@ Where o.status = 'Paid'
 
 
 -- Bonus
-Select c.id, c.plate, c.plate_country, count(*)
-From car_owner c
-Join offence_case o
+Select c.id, c.plate, c.plate_country
+From dbo.car_owner c
+Join dbo.offence_case o
 On c.id = o.owner_id
 Where o.status = 'Paid'
 Group by c.id, c.plate, c.plate_country
