7e06a98178fe326905dae881dec2ccf2d4fe0b07
ANH2510 <nguyenlexuananh2510@gmail.com>
2022-04-13 14:04:10 +0200

    sql

--- challenge_2.sql
+++ challenge_2.sql
@@ -1,13 +1,15 @@
 Select * 
-From car_owner c
-Join offence_case o
+From dbo.car_owner c
+Join dbo.offence_case o
 On c.id = o.owner_id
-Where o.status = "Paid"
+Where o.status = 'Paid'
 
+
+-- Bonus
 Select c.id, c.plate, c.plate_country, count(*)
 From car_owner c
 Join offence_case o
 On c.id = o.owner_id
-Where o.status = "Paid"
+Where o.status = 'Paid'
 Group by c.id, c.plate, c.plate_country
-Having Count(*) > 2
+Having Count(*) >= 2
