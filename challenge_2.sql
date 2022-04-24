88b6478c823a00b44ecb063ee1175db447dc4303
ANH2510 <nguyenlexuananh2510@gmail.com>
2022-04-24 20:37:36 +0200

    1

--- challenge_2.sql
+++ challenge_2.sql
@@ -1,3 +1,5 @@
+-- write a SQL query to select all paid offences of international (non German) car owners.
+
 Select * 
 From dbo.car_owner c
 Join dbo.offence_case o
@@ -5,7 +7,7 @@ On c.id = o.owner_id
 Where o.status = 'Paid'
 
 
--- Bonus
+-- How would the query look like if you would only like to select paid cases of offenders who have caused at least two paid offences?
 Select c.id, c.plate, c.plate_country
 From dbo.car_owner c
 Join dbo.offence_case o
